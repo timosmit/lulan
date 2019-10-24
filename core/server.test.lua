73ce291708472dfa2f03f5efc2f18e975a51ad29
Adam Klvač <adam@klva.cz>
2019-10-25 00:24:26 +0200

    server: Fixed restart argument cast in test.

--- core/server.test.lua
+++ core/server.test.lua
@@ -6,8 +6,8 @@ local server = require('server')
 
 local init
 server.on('init', function(...) init = arg end)
-et_InitGame(1, 2, 3)
-assert(init[1] == 1 and init[2] == 2 and init[3] == 3)
+et_InitGame(1, 2, 1)
+assert(init[1] == 1 and init[2] == 2 and init[3] == true)
 
 local sent = {}
 
